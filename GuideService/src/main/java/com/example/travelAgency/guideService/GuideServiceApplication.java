13ff496e14e43c4330fbbda04f7dd44d0e7cec3a
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-10-30 00:46:44 +0530

    update

---
+++ GuideService/src/main/java/com/example/travelAgency/guideService/GuideServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.travelAgency.guideService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuideServiceApplication.class, args);
+    }
+}
