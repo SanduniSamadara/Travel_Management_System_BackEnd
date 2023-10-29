2fbcfb0eb4658e9ddfa1796a41b73df1acea6dd9
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-10-30 00:18:22 +0530

    update

---
+++ TravelService/src/main/java/com/example/travelAgency/travelService/TravelServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.travelAgency.travelService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelServiceApplication.class, args);
+    }
+}
