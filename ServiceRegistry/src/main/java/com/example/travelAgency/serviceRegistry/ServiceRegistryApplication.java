3378173aaa80ece9859762d246b9394bf19fe318
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-10-30 01:14:19 +0530

    update

---
+++ ServiceRegistry/src/main/java/com/example/travelAgency/serviceRegistry/ServiceRegistryApplication.java
@@ -0,0 +1,11 @@
+package com.example.travelAgency.serviceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+}
